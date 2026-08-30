3937def1bb7f94d10e47c8935bc347b96969257b
Sandesh <sandeshatdiscord@gmail.com>
2026-08-31 00:03:31 +0530

    Change server listen address to '0.0.0.0'

--- server/index.js
+++ server/index.js
@@ -44,4 +44,6 @@ app.get('/', (req, res) => res.json({ message: 'Server is running!' }));
 chatHandler(io);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
